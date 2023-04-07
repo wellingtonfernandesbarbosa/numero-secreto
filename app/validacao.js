00175d6add8cbb270ae6be561db270714a8ab166
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-04-07 10:14:10 -0300

    Criando a lógica do jogo
    Adicionada verificação de validade do chute do usuário e opção de jogar novamente. A função "verificaSeOChutePossuiUmValorValido" verifica se o chute é válido e atualiza a tela com a resposta correspondente. Duas funções auxiliares verificam se o chute é inválido ou se está fora do intervalo permitido. O evento de clique no botão "jogar-novamente" atualiza a página para iniciar um novo jogo.

---
+++ app/validacao.js
@@ -0,0 +1,39 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute;
+    
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOoValorPermitido(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você Acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</div>
+        <button id="jogar-novamente" class="btn-jogar"></button>
+        `
+        return
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+    };
+};
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+};
+
+function numeroForMaiorOuMenorQueOoValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor;
+};
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == jogar-novamente){
+        window.location.reload();
+    }
+});
